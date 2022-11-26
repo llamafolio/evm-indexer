efb9a4a8194ae773c908c0cbb146c42cc2635251
eabz <eabz@kindynos.mx>
2022-11-26 15:41:01 -0600

    Add more workers and less batches and spawn a new task for each worker

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -13,77 +13,75 @@ pub async fn fetch_blocks(rpc: &Rpc, db: &Database, config: Config) -> Result<()
 
     let db_blocks_set = vec_to_set(db.get_block_numbers().await.unwrap());
 
-    let missing_blocks_set: Vec<i64> = full_blocks_set
+    let missing_blocks: Vec<i64> = full_blocks_set
         .into_iter()
         .filter(|n| !db_blocks_set.contains(n))
         .collect();
 
+    let missing_blocks_amount = missing_blocks.len();
+
     info!(
         "Fetching {} blocks with batches of {} blocks with {} workers",
-        missing_blocks_set.len(),
-        config.batch_size,
-        config.workers
+        missing_blocks_amount, config.batch_size, config.workers
     );
 
-    for work_chunk in missing_blocks_set.chunks(config.batch_size * config.workers) {
-        let mut works = vec![];
+    let mut workers = Vec::new();
 
-        let chunks = work_chunk.chunks(config.batch_size);
+    for worker_chunk in missing_blocks.chunks(missing_blocks_amount / config.workers) {
+        let worker_chunk_vec: Vec<i64> = worker_chunk.to_vec();
 
-        info!(
-            "Procesing chunk from block {} to {} for chain {}",
-            work_chunk.first().unwrap(),
-            work_chunk.last().unwrap(),
-            config.chain.clone()
-        );
+        let db = db.clone();
+        let rpc = rpc.clone();
+        let chain = config.clone().chain;
 
-        for worker_part in chunks {
-            works.push(rpc.get_blocks(worker_part.to_vec()));
-        }
+        let worker = tokio::spawn(async move {
+            let chunks = worker_chunk_vec.chunks(config.batch_size);
 
-        let block_responses = join_all(works).await;
+            info!(
+                "Procesing chunk from block {} to {} for chain {}",
+                worker_chunk_vec.first().unwrap(),
+                worker_chunk_vec.last().unwrap(),
+                chain
+            );
 
-        let res = block_responses.into_iter().map(Result::unwrap);
+            for worker_part in chunks {
+                let (
+                    db_blocks,
+                    db_txs,
+                    db_tx_receipts,
+                    db_tx_logs,
+                    db_contract_creation,
+                    db_contract_interaction,
+                    db_token_transfers,
+                ) = rpc.get_blocks(worker_part.to_vec()).await.unwrap();
 
-        if res.len() < config.workers {
-            info!("Incomplete result returned, omitting...")
-        }
+                if db_blocks.len() < config.batch_size {
+                    info!("Incomplete blocks returned, omitting...");
+                    continue;
+                }
 
-        let mut stores = vec![];
+                if db_txs.len() != db_tx_receipts.len() {
+                    info!("Txs and txs_receipts don't match, omitting...");
+                    continue;
+                }
 
-        for (
-            db_blocks,
-            db_txs,
-            db_tx_receipts,
-            db_tx_logs,
-            db_contract_creation,
-            db_contract_interaction,
-            db_token_transfers,
-        ) in res
-        {
-            if db_blocks.len() < config.batch_size {
-                info!("Incomplete blocks returned, omitting...");
-                continue;
+                db.store_blocks_and_txs(
+                    db_blocks,
+                    db_txs,
+                    db_tx_receipts,
+                    db_tx_logs,
+                    db_contract_creation,
+                    db_contract_interaction,
+                    db_token_transfers,
+                )
+                .await;
             }
+        });
 
-            if db_txs.len() != db_tx_receipts.len() {
-                info!("Txs and txs_receipts don't match, omitting...");
-                continue;
-            }
-
-            stores.push(db.store_blocks_and_txs(
-                db_blocks,
-                db_txs,
-                db_tx_receipts,
-                db_tx_logs,
-                db_contract_creation,
-                db_contract_interaction,
-                db_token_transfers,
-            ));
-        }
-
-        join_all(stores).await;
+        workers.push(worker);
     }
+
+    join_all(workers).await;
 
     Ok(())
 }
