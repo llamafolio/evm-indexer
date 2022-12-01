f98caa2d3f600a0ee5a85436bb283a1123ec11fa
eabz <eabz@kindynos.mx>
2022-12-01 13:22:31 -0600

    Use btree for all indexes

--- migrations/2022-12-01-182340_create_indexes/up.sql
+++ migrations/2022-12-01-182340_create_indexes/up.sql
@@ -1,23 +1,23 @@
 CREATE INDEX IF NOT EXISTS token_transfers_by_hash 
-ON token_transfers USING HASH (hash);
+ON token_transfers (hash);
 
 CREATE INDEX IF NOT EXISTS token_transfers_by_sender
-ON token_transfers USING HASH (from_address);
+ON token_transfers (from_address);
 
 CREATE INDEX IF NOT EXISTS token_transfers_by_receiver
-ON token_transfers USING HASH (to_address);
+ON token_transfers (to_address);
 
 CREATE INDEX IF NOT EXISTS tokens_by_chain 
-ON tokens USING HASH (chain);
+ON tokens (chain);
 
 CREATE INDEX IF NOT EXISTS tokens_by_address 
-ON tokens USING HASH (address);
+ON tokens (address);
 
 CREATE INDEX IF NOT EXISTS txs_by_block_number 
-ON txs USING BTREE (block_number DESC);
+ON txs (block_number DESC);
 
 CREATE INDEX IF NOT EXISTS txs_by_sender 
-ON txs USING HASH (from_address);
+ON txs (from_address);
 
 CREATE INDEX IF NOT EXISTS txs_by_receiver
-ON txs USING HASH (to_address);
+ON txs (to_address);
