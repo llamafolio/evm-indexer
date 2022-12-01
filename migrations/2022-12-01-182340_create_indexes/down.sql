14eee8953ceb363528d0826f41da14e14829e2c2
eabz <eabz@kindynos.mx>
2022-12-01 13:02:46 -0600

    Fix down migration for indexes

--- migrations/2022-12-01-182340_create_indexes/down.sql
+++ migrations/2022-12-01-182340_create_indexes/down.sql
@@ -1,4 +1,6 @@
 DROP INDEX  token_transfers_by_hash;
+DROP INDEX  token_transfers_by_sender;
+DROP INDEX  token_transfers_by_receiver;
 DROP INDEX  tokens_by_chain;
 DROP INDEX tokens_by_address;
 DROP INDEX  txs_by_block_number;
