cccb3c9ad470ef3040d810bc713cb459b7453e4c
eabz <eabz@kindynos.mx>
2022-12-28 13:05:55 -0700

    Mix single fetcher with multiple workers

--- src/fetcher.rs
+++ src/fetcher.rs
@@ -63,74 +63,90 @@ pub async fn fetch_blocks_singles(db: &Database, config: &Config, rpc: &Rpc) -> 
         missing_blocks_amount, config.batch_size
     );
 
-    for block in missing_blocks {
-        let (
-            db_block,
-            mut db_txs,
-            db_tx_receipts,
-            db_tx_logs,
-            db_contract_creation,
-            db_contract_interaction,
-            db_token_transfers,
-        ) = rpc.get_block(&config, block).await.unwrap();
+    let single_fetch_worker_batch = missing_blocks.chunks(config.workers);
 
-        let db_txs_count = db_txs.len();
-        let db_tx_receipts_count = db_tx_receipts.len();
+    let mut works = vec![];
+    for worker_chunk in single_fetch_worker_batch {
+        let worker = tokio::spawn({
+            let config = config.clone();
+            let rpc = rpc.clone();
+            let db = db.clone();
+            let chunk = worker_chunk.to_vec();
 
-        let mut enough_receipts = true;
+            async move {
+                for block in chunk {
+                    let (
+                        db_block,
+                        mut db_txs,
+                        db_tx_receipts,
+                        db_tx_logs,
+                        db_contract_creation,
+                        db_contract_interaction,
+                        db_token_transfers,
+                    ) = rpc.get_block(&config, block.clone()).await.unwrap();
 
-        if db_txs_count != db_tx_receipts_count {
-            info!(
-                "Not enough receipts for batch: txs({}) receipts ({})",
-                db_txs_count, db_tx_receipts_count,
-            );
+                    let db_txs_count = db_txs.len();
+                    let db_tx_receipts_count = db_tx_receipts.len();
 
-            enough_receipts = false;
-        }
+                    let mut enough_receipts = true;
 
-        if !enough_receipts {
-            let db_receipts_hash: HashSet<String> = vec_string_to_set(
-                db_tx_receipts
-                    .clone()
-                    .into_iter()
-                    .map(|receipt| receipt.hash)
-                    .collect(),
-            );
+                    if db_txs_count != db_tx_receipts_count {
+                        info!(
+                            "Not enough receipts for batch: txs({}) receipts ({})",
+                            db_txs_count, db_tx_receipts_count,
+                        );
 
-            let mut db_txs_with_no_receipts: Vec<DatabaseTxNoReceipt> = vec![];
+                        enough_receipts = false;
+                    }
 
-            for tx in &mut db_txs {
-                let hash = tx.hash.clone();
-                let chain = tx.chain.clone();
-                if !db_receipts_hash.contains(&hash) {
-                    db_txs_with_no_receipts.push(DatabaseTxNoReceipt {
-                        hash,
-                        chain,
-                        block_number: tx.block_number,
-                    });
+                    if !enough_receipts {
+                        let db_receipts_hash: HashSet<String> = vec_string_to_set(
+                            db_tx_receipts
+                                .clone()
+                                .into_iter()
+                                .map(|receipt| receipt.hash)
+                                .collect(),
+                        );
+
+                        let mut db_txs_with_no_receipts: Vec<DatabaseTxNoReceipt> = vec![];
+
+                        for tx in &mut db_txs {
+                            let hash = tx.hash.clone();
+                            let chain = tx.chain.clone();
+                            if !db_receipts_hash.contains(&hash) {
+                                db_txs_with_no_receipts.push(DatabaseTxNoReceipt {
+                                    hash,
+                                    chain,
+                                    block_number: tx.block_number,
+                                });
+                            }
+                        }
+
+                        info!(
+                            "Storing {} txs with no receipt for future check",
+                            db_txs_with_no_receipts.len(),
+                        );
+
+                        db.store_txs_no_receipt(&db_txs_with_no_receipts).await;
+                    }
+
+                    db.store_blocks_and_txs(
+                        vec![db_block],
+                        db_txs,
+                        db_tx_receipts,
+                        db_tx_logs,
+                        db_contract_creation,
+                        db_contract_interaction,
+                        db_token_transfers,
+                    )
+                    .await;
                 }
             }
-
-            info!(
-                "Storing {} txs with no receipt for future check",
-                db_txs_with_no_receipts.len(),
-            );
-
-            db.store_txs_no_receipt(&db_txs_with_no_receipts).await;
-        }
-
-        db.store_blocks_and_txs(
-            vec![db_block],
-            db_txs,
-            db_tx_receipts,
-            db_tx_logs,
-            db_contract_creation,
-            db_contract_interaction,
-            db_token_transfers,
-        )
-        .await;
+        });
+        works.push(worker)
     }
 
+    join_all(works).await;
     Ok(())
 }
 
