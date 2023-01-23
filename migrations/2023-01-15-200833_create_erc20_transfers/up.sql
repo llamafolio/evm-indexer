c938838a00d667e1e29a8f7fd2ee949d5cfcb590
Mark Shields <4237425+beejiujitsu@users.noreply.github.com>
2023-01-23 15:14:46 -0500

    fix DatabaseError(Unknown, "syntax error at or near \";\"")

--- migrations/2023-01-15-200833_create_erc20_transfers/up.sql
+++ migrations/2023-01-15-200833_create_erc20_transfers/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE evm_erc20_transfers (
-  hash VARCHAR NOT NULL, 
+  hash VARCHAR NOT NULL,
   log_index BIGINT NOT NULL,
   token VARCHAR NOT NULL,
   from_address VARCHAR NOT NULL,
   to_address VARCHAR NOT NULL,
   value VARCHAR NOT NULL,
-  CONSTRAINT erc20_transfers_table_pk PRIMARY KEY (hash, log_index);
+  CONSTRAINT erc20_transfers_table_pk PRIMARY KEY (hash, log_index)
 );
